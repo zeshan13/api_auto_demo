bfe9a215cf8b46800f1ad76a44f92bb50bcca071
z_erduo <1220237603@qq.com>
2021-06-19 00:58:09 +0800

    update cases assert and clear datas

--- api_auto_demo/test_api.py
+++ api_auto_demo/test_api.py
@@ -3,13 +3,16 @@
 # @Author  : zeshan
 # @File    : test_api.py
 import json
+import pytest
 import requests
+from tools import Tools
 
 
 class TestAPI:
     def setup_class(self):
-        corpid = "XXXXXXXXXXX"
-        corpsecret = "XXXXXXXXXXX"
+        # 获取token
+        corpid = ""
+        corpsecret = ""
         url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
         params = {
             "corpid": corpid,
@@ -17,8 +20,33 @@ class TestAPI:
         }
 
         res = requests.get(url=url, params=params)
+        print("url:%s;params:%s" % (url, params))
         print(json.dumps(res.json(), indent=2, ensure_ascii=False))
         self.token = res.json()["access_token"]
+
+        self.t = Tools()
+
+        # 清理数据
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list"
+        params = {
+            "access_token": self.token
+        }
+        res = requests.post(url=url, params=params)
+        print("url:%s;params:%s" % (url, params))
+        print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+        rsp = res.json()
+        tag_list = [tag["id"] for group in rsp["tag_group"] for tag in group["tag"]]
+
+        if len(tag_list) > 2:
+            url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag"
+            params = {"access_token": self.token}
+            json_datas = {
+                "tag_id": tag_list[2:]
+            }
+
+            res = requests.post(url=url, params=params, json=json_datas)
+            print("url:%s;params:%s;datas:%s" % (url, params, json_datas))
+            print(json.dumps(res.json(), indent=2, ensure_ascii=False))
 
     def test_get_corp_tag(self):
         url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list"
@@ -26,30 +54,128 @@ class TestAPI:
             "access_token": self.token
         }
         res = requests.post(url=url, params=params)
+        print("url:%s;params:%s" % (url, params))
         print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+        rsp = res.json()
+        assert rsp["errcode"] == 0
 
     def test_add_corp_tag(self):
+        tag_count = self.t.get_init_data("tag_count", "./init_datas_count.yaml")
+        group_count = self.t.get_init_data("tag_count", "./init_datas_count.yaml")
         url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag"
         params = {"access_token": self.token}
         json_datas = {
-            "group_name": "GROUP_NAME",
+            "group_name": "GROUP_NAME_" + str(group_count),
             "tag": [{
-                "name": "TAG_NAME_1",
-                "order": 1
+                "name": "TAG_NAME_" + str(tag_count)
             }]
         }
 
         res = requests.post(url=url, params=params, json=json_datas)
+        print("url:%s;params:%s;datas:%s" % (url, params, json_datas))
         print(json.dumps(res.json(), indent=2, ensure_ascii=False))
 
+        rsp = res.json()
+        assert rsp["errcode"] == 0
+        assert rsp["tag_group"]["group_name"] == json_datas["group_name"]
+        assert rsp["tag_group"]["tag"][0]["name"] == json_datas["tag"][0]["name"]
+
+    def test_edit_corp_tag(self):
+        # 调用add_corp_tag新增标签，获取tag_id
+        tag_count = self.t.get_init_data("tag_count", "./init_datas_count.yaml")
+        group_count = self.t.get_init_data("tag_count", "./init_datas_count.yaml")
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag"
+        params = {"access_token": self.token}
+        json_datas = {
+            "group_name": "GROUP_NAME_" + str(group_count),
+            "tag": [{
+                "name": "TAG_NAME_" + str(tag_count)
+            }]
+        }
+
+        res = requests.post(url=url, params=params, json=json_datas)
+        print("url:%s;params:%s;datas:%s" % (url, params, json_datas))
+        print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+        tag_id = res.json()["tag_group"]["tag"][0]["id"]
+
+        # eidt
+        tag_count = self.t.get_init_data("tag_count", "./init_datas_count.yaml")
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/edit_corp_tag"
+        params = {"access_token": self.token}
+        json_datas = {
+            "id": tag_id,
+            "name": "NEW_TAG_NAME" + str(tag_count),
+        }
+        res = requests.post(url=url, params=params, json=json_datas)
+        print("url:%s;params:%s;datas:%s" % (url, params, json_datas))
+        print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+
+        rsp = res.json()
+        assert rsp["errcode"] == 0
+
+        # 调用get_corp_tag_list获取修改后的标签名
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list"
+        params = {
+            "access_token": self.token
+        }
+        json_datas_1 = {
+            "tag_id":
+                [
+                    tag_id
+                ],
+        }
+        res = requests.post(url=url, params=params, json=json_datas_1)
+        print("url:%s;params:%s;datas:%s" % (url, params, json_datas_1))
+        print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+        rsp = res.json()
+        # 断言 tag_name是否与传入的一致
+        assert rsp["tag_group"][0]["tag"][0]["name"] == json_datas["name"]
+
     def test_del_corp_tag(self):
+        # 调用add_corp_tag新增标签，获取tag_id
+        tag_count = self.t.get_init_data("tag_count", "./init_datas_count.yaml")
+        group_count = self.t.get_init_data("tag_count", "./init_datas_count.yaml")
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag"
+        params = {"access_token": self.token}
+        json_datas = {
+            "group_name": "GROUP_NAME_" + str(group_count),
+            "tag": [{
+                "name": "TAG_NAME_" + str(tag_count)
+            }]
+        }
+
+        res = requests.post(url=url, params=params, json=json_datas)
+        print("url:%s;params:%s;datas:%s" % (url, params, json_datas))
+        print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+        tag_id = res.json()["tag_group"]["tag"][0]["id"]
+
+        # del
         url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag"
         params = {"access_token": self.token}
         json_datas = {
             "tag_id": [
-                "etjPoZCgAAAsRevEUFbGWlp-S6Gkvlgg"
+                tag_id
             ]
         }
 
         res = requests.post(url=url, params=params, json=json_datas)
+        print("url:%s;params:%s;datas:%s" % (url, params, json_datas))
         print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+
+        # 调用get_corp_tag_list获删除后的tag信息，断言deleted字段
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list"
+        params = {
+            "access_token": self.token
+        }
+        json_datas_1 = {
+            "tag_id":
+                [
+                    tag_id
+                ],
+        }
+        res = requests.post(url=url, params=params, json=json_datas_1)
+        print("url:%s;params:%s;datas:%s" % (url, params, json_datas_1))
+        print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+        rsp = res.json()
+        # 断言get_corp_tag_list获取到的deleted字段
+        assert rsp["tag_group"][0]["deleted"] == True
---
+++ api_auto_demo/tools.py
@@ -0,0 +1,30 @@
+import yaml
+
+import yaml
+
+
+class Tools:
+    def read_yaml(self, file_path):
+        with open(file_path, encoding='utf-8') as f:
+            datas = yaml.safe_load(f)
+        return datas
+
+    def write_yaml(self, file_path, content):
+        with open(file_path, "w", encoding="UTF-8") as f:
+            yaml.dump(content, f)
+
+    def get_init_data(self, key, file_path):
+        datas = self.read_yaml(file_path)
+        datas[key] += 1
+        self.write_yaml(file_path, datas)
+        return datas[key]
+
+
+if __name__ == '__main__':
+    t = Tools()
+    file_path = "init_datas_count.yaml"
+    init_data = t.read_yaml(file_path)
+    print(init_data)
+
+    a = t.get_init_data("group_count", file_path)
+    print(a)
